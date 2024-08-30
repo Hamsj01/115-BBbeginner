31aeaa2ce2f01dd78a20cba533d3099d722bae66
Miraç <mirackatmer123@gmail.com>
2024-08-30 23:55:39 +0300

    ders tekrarı 3. hafta

---
+++ 2.Ders C#/Project07_NumberMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Projec07_NumberMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Aritmetik Operatörler
+        int a = 10;
+        int b = 4;
+
+        int result;
+        result = a + b;
+        result = a - b;
+        result = a * b;
+        result = a / b;
+        result = a % b;
+
+        result = Math.Abs(-10);   //Abs: Mutlak değer.
+        result = (int)Math.Pow(4, 5);   //Pow: (TABAN, ÜS)
+        double result2;
+        result2 = Math.Sqrt(16);  //Karekökü alınır.
+        result2 = Math.Round(7.25);
+        result2 = Math.Round(7.51);  //En yakın tam sayıya yuvarlar (Round)
+        result2 = Math.Ceiling(7.25);  //Yukarı doğru yuvarla (Ceiling)
+        result2 = Math.Floor(7.99);   // Aşağı yuvarla (Floor)
+
+        //Math.Tan, Math.Sin, Math.Cos...
+        //Bu kodun çıktısı en son result2 Mat.Floor(7.99); olduğu için çıktı:7
+
+
+        System.Console.WriteLine(result2);
+        #endregion
+    }
+}
---
+++ 2.Ders C#/Project08_DateTimes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Project08_DateTimes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime now = DateTime.Now;
+
+        // System.Console.WriteLine($"Şu anki tarih/saat: {now}"); //çıktı: Şu anki tarih/saat: 30.08.2024 20:56:09
+        // System.Console.WriteLine($"3 hafta sonraki tarih: {now.AddDays(21)}");
+        // System.Console.WriteLine($"3 ay sonraki tarih: {now.AddMonths(3)}"); //çıktı: 30.11.2024 21:01:18
+
+        // System.Console.WriteLine(now.ToString("dd/MM/yyyy")); //çıktı: 30.08.2024
+        // System.Console.WriteLine(now.ToString("HH:mm:ss")); // çıktı: şu anki saat 21:04:33
+        // System.Console.WriteLine(now.ToString("d")); // çıktı: 30.08.2024
+        // System.Console.WriteLine(now.ToString("D")); // çıktı: 30 Ağustos 2024 Cuma
+
+        // System.Console.WriteLine(now.ToString("MMMM yyyy")); //çıktı: Ağustos 2024
+
+        // System.Console.WriteLine(now.ToShortDateString());
+        // System.Console.WriteLine(now.ToShortTimeString());
+
+        int day=5;
+        int mounth=6;
+        int year=1988;
+        DateTime date = new DateTime(year, month, day, 4, 5, 0);
+        System.Console.WriteLine(date);
+        System.Console.WriteLine(date.AddMonths(-3));
+
+
+
+
+
+
+
+    }
+}
